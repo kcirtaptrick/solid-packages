9d775f17a201180e6bdd40653068c5a139a2dd82
Patrick Wang <patrick.bohan.wang@gmail.com>
2023-01-13 17:06:24 -0800

    Fix export

--- packages/solid-u/src/index.ts
+++ packages/solid-u/src/index.ts
@@ -1,5 +1,5 @@
 export { default as splitAccessor } from "./splitAccessor/index.js";
-export { default as handleEvent } from "./eventHandler/index.js";
+export { default as eventHandler } from "./eventHandler/index.js";
 export { default as reactiveCssVars } from "./reactiveCssVars/index.js";
 export { default as reactiveProps } from "./reactiveProps/index.js";
 export * from "./reactiveProps/index.js";
