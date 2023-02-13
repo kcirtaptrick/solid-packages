98f63f6e59013815fd5ceb82c4a991e67b31b466
Patrick Wang <patrick.bohan.wang@gmail.com>
2023-02-12 17:52:38 -0800

    solid-u: Export signalValuePromise

--- packages/solid-u/src/index.ts
+++ packages/solid-u/src/index.ts
@@ -1,4 +1,5 @@
 export { default as splitAccessor } from "./splitAccessor/index.js";
+export { default as signalValuePromise } from "./signalValuePromise/index.js";
 export { default as eventHandler } from "./eventHandler/index.js";
 export { default as reactiveCssVars } from "./reactiveCssVars/index.js";
 export { default as reactiveProps } from "./reactiveProps/index.js";
