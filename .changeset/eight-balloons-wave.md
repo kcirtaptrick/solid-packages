befec2afa4825714d60e629e7477d09519278301
Patrick Wang <patrick.bohan.wang@gmail.com>
2023-11-20 21:59:23 -0800

    solid-u: EventFrom

---
+++ .changeset/eight-balloons-wave.md
@@ -0,0 +1,5 @@
+---
+"solid-u": patch
+---
+
+Add EventFrom type
--- packages/solid-u/src/eventHandler/eventHandler.ts
+++ packages/solid-u/src/eventHandler/eventHandler.ts
@@ -1,11 +1,16 @@
-import { JSX } from "solid-js";
+import { ComponentProps, JSX, ValidComponent } from "solid-js";
 
 export default function eventHandler<Elem, Ev extends Event>(
   onEvent: JSX.EventHandlerUnion<Elem, Ev> | undefined
 ): JSX.EventHandler<Elem, Ev> {
   if (!onEvent) return () => {};
 
-  if ("0" in onEvent) return (e) => onEvent[0](onEvent[1], e);
+  if (typeof onEvent === "function") return onEvent;
 
-  return onEvent;
+  return (e) => onEvent[0](onEvent[1], e);
 }
+
+export type EventFrom<
+  T extends ValidComponent,
+  E extends Extract<keyof ComponentProps<T>, `on${string}`>
+> = Parameters<ComponentProps<T>[E]>[0];
